70381fd6c6b8d1582e048dffb2a3b1b207b4c9e5
Gittenger <36045366+Gittenger@users.noreply.github.com>
2022-01-23 02:19:15 -0500

    import menu btn, custom for proj

--- src/components/components.index.js
+++ src/components/components.index.js
@@ -1,7 +1,8 @@
+import HamburgerMenu from "./General/HamburgerMenu.component.jsx"
 import Header from "./General/Header.component.jsx"
 import Layout from "./General/Layout.component.jsx"
 export const CIndex = {
-		Header,Layout
+		HamburgerMenu,Header,Layout
 	}
 	export default CIndex
 	
