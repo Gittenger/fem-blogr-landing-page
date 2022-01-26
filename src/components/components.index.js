4d726c9297eadb55b458d32e326be1b9f17b2913
Gittenger <36045366+Gittenger@users.noreply.github.com>
2022-01-26 02:25:00 -0500

    mv header to features

--- src/components/components.index.js
+++ src/components/components.index.js
@@ -1,14 +1,21 @@
-import Footer from "./Features/Footer.component.jsx"
-import SectionOne from "./Features/SectionOne.component.jsx"
-import SectionThree from "./Features/SectionThree.component.jsx"
-import SectionTwo from "./Features/SectionTwo.component.jsx"
-import Btn from "./General/Btn.component.jsx"
-import HamburgerMenu from "./General/HamburgerMenu.component.jsx"
-import Header from "./General/Header.component.jsx"
-import Layout from "./General/Layout.component.jsx"
-import MobileNav from "./General/MobileNav.component.jsx"
+import Footer from './Features/Footer.component.jsx'
+import SectionOne from './Features/SectionOne.component.jsx'
+import SectionThree from './Features/SectionThree.component.jsx'
+import SectionTwo from './Features/SectionTwo.component.jsx'
+import Btn from './General/Btn.component.jsx'
+import HamburgerMenu from './General/HamburgerMenu.component.jsx'
+import Header from './Features/Header.component.jsx'
+import Layout from './General/Layout.component.jsx'
+import MobileNav from './General/MobileNav.component.jsx'
 export const CIndex = {
-		Footer,SectionOne,SectionThree,SectionTwo,Btn,HamburgerMenu,Header,Layout,MobileNav
-	}
-	export default CIndex
-	
+  Footer,
+  SectionOne,
+  SectionThree,
+  SectionTwo,
+  Btn,
+  HamburgerMenu,
+  Header,
+  Layout,
+  MobileNav,
+}
+export default CIndex
