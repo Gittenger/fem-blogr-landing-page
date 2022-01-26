c34759abf1627672d6a9d418891e22be4a657269
Gittenger <36045366+Gittenger@users.noreply.github.com>
2022-01-26 01:14:53 -0500

    secTwo desk good

--- src/components/Features/SectionTwo.component.jsx
+++ src/components/Features/SectionTwo.component.jsx
@@ -7,17 +7,22 @@ const { PhonesImg } = images
 const SectionTwo = () => {
   return (
     <>
-      <div className={`relative z-40 -mb-52 ${styles.phonesImg}`}>
+      <div className={`relative z-40 -mb-52 md:hidden`}>
         <img className="" src={PhonesImg} alt="" />
       </div>
-      <div className="flex-col justify-center items-center relative rounded-tr-4xl rounded-bl-4xl bg-gradient-to-tl from-blue-gray-desat to-blue-gray-800 overflow-hidden">
-        <div className={`absolute w-full h-full z-30 ${styles.bgImg}`}></div>
+      <div className="flex-col justify-center items-center relative rounded-tr-4xl rounded-bl-4xl bg-gradient-to-tl from-blue-gray-desat to-blue-gray-800 overflow-hidden md:overflow-visible md:grid grid-cols-2">
+        <div className={`absolute w-full h-full ${styles.bgImg}`}></div>
+        <div
+          className={`hidden md:block absolute z-40 col-start-1 col-end-2 ${styles.phonesImg}`}
+        >
+          <img className="" src={PhonesImg} alt="" />
+        </div>
 
-        <div className="relative text-white pt-56 pb-28 w-full  text-center z-40">
+        <div className="relative text-white pt-56 pb-28 w-full text-center z-40 col-start-2 col-end-3 md:text-left md:py-36">
           <h2 className="text-4xl font-heading font-semibold mb-5">
             State of the Art Infrastructure
           </h2>
-          <p className="px-9 leading-relaxed font-heading font-light">
+          <p className="px-9 leading-relaxed font-heading font-light md:pl-0 md:pr-36">
             With reliability and speed in mind, worldwide data centers provide
             the backbone for ultra-fast connectivity. This ensures your site
             will load instantly, no matter where your readers are, keeping your
