630aae6587a141a89255276b7cff935af50af32e
Gittenger <36045366+Gittenger@users.noreply.github.com>
2022-01-26 18:28:46 -0500

    active states ok

--- src/components/General/Btn.component.jsx
+++ src/components/General/Btn.component.jsx
@@ -4,14 +4,14 @@ import styles from './styles/Btn.module.css'
 const Btn = ({ textContent, transparent, gradient, sm }) => {
   return (
     <button
-      className={` font-bold py-2.5 px-4 rounded-3xl border-white border-2 duration-300 ${
+      className={` font-bold py-3 px-4 rounded-3xl duration-300 ${
         sm ? styles.btnSm : styles.btn
       } ${
         transparent
-          ? 'bg-transparent text-white hover:bg-white hover:text-red-300'
+          ? 'bg-transparent text-white ring-2 ring-inset ring-white hover:bg-white hover:text-red-300'
           : gradient
-          ? ' text-white bg-gradient-to-r from-red-100 to-red-300 hover:text-blue-gray-800'
-          : 'bg-white text-red-300 hover:bg-transparent hover:text-white'
+          ? ' text-white bg-gradient-to-r from-red-100 to-red-300 hover:text-blue-gray-800 hover:shadow-lg'
+          : 'bg-white text-red-300 hover:bg-red-50 hover:text-white border-none'
       }`}
     >
       {textContent}
