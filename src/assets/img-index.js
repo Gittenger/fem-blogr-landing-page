ab298c479b727b857508aa3e1b559ba4841261e2
Gittenger <36045366+Gittenger@users.noreply.github.com>
2022-01-26 01:52:31 -0500

    secThree desk good

--- src/assets/img-index.js
+++ src/assets/img-index.js
@@ -3,6 +3,7 @@ import BgIntroMob from './img/bg-pattern-intro-mobile.svg'
 import EditorMob from './img/illustration-editor-mobile.svg'
 import EditorDesk from './img/illustration-editor-desktop.svg'
 import LaptopMob from './img/illustration-laptop-mobile.svg'
+import LaptopDesk from './img/illustration-laptop-desktop.svg'
 import PhonesImg from './img/illustration-phones.svg'
 import ArrowDark from './img/icon-arrow-dark.svg'
 
@@ -12,6 +13,7 @@ export const images = {
   EditorMob,
   EditorDesk,
   LaptopMob,
+  LaptopDesk,
   PhonesImg,
   ArrowDark,
 }
--- src/components/Features/SectionThree.component.jsx
+++ src/components/Features/SectionThree.component.jsx
@@ -1,33 +1,44 @@
 import React from 'react'
 import images from '../../assets/img-index'
+import styles from './styles/SectionThree.module.css'
 
-const { LaptopMob } = images
+const { LaptopMob, LaptopDesk } = images
 
 const headingText =
-  'text-2xl font-heading font-semibold text-blue-800 mb-3.5 text-center w-3/5'
+  'text-2xl font-heading font-semibold text-blue-800 mb-3.5 text-center w-3/5 md:text-left md:w-full'
 const pText =
-  'px-10 text-center leading-relaxed tracking-wider font-light font-heading text-blue-gray-800'
+  'px-10 text-center leading-relaxed tracking-wider font-light font-heading text-blue-gray-800 md:text-left md:px-0 md:pr-24 lg:pr-36'
 
 const SectionThree = () => {
   return (
-    <div className="flex flex-col justify-center items-center pt-20 pb-12">
-      <div className="mb-14">
-        <img className="w-full" src={LaptopMob} alt="" />
+    <div
+      className="relative flex flex-col justify-center items-center pt-20 pb-12 md:pt-56 md:pb-52  
+     lg:pt-80 lg:pb-96  md:grid md:grid-cols-2"
+    >
+      <div
+        className={`mb-14 md:absolute md:top-24 lg:top-28 col-start-1 col-end-2 ${styles.imgWrapper}`}
+      >
+        <picture>
+          <source srcSet={LaptopDesk} media="(min-width: 1024px)" />
+          <img className="w-full" src={LaptopMob} alt="" />
+        </picture>
       </div>
-      <h2 className={` ${headingText}`}>Free, open, simple</h2>
-      <p className={`mb-12 ${pText}`}>
-        Blogr is a free and open source application backed by a large community
-        of helpful developers. It supports features such as code syntax
-        highlighting, RSS feeds, social media integration, third-party
-        commenting tools, and works seamlessly with Google Analytics. The
-        architecture is clean and is relatively easy to learn.
-      </p>
-      <h2 className={` ${headingText}`}>Batteries included</h2>
-      <p className={`mb-12 ${pText}`}>
-        Batteries included. We built a simple and straightforward CLI tool that
-        makes customization and deployment a breeze, but capable of producing
-        even the most complicated sites.
-      </p>
+      <div className="flex flex-col justify-center items-center col-start-2 col-end-3">
+        <h2 className={` ${headingText}`}>Free, open, simple</h2>
+        <p className={`mb-12 ${pText}`}>
+          Blogr is a free and open source application backed by a large
+          community of helpful developers. It supports features such as code
+          syntax highlighting, RSS feeds, social media integration, third-party
+          commenting tools, and works seamlessly with Google Analytics. The
+          architecture is clean and is relatively easy to learn.
+        </p>
+        <h2 className={` ${headingText}`}>Batteries included</h2>
+        <p className={`mb-12 ${pText}`}>
+          Batteries included. We built a simple and straightforward CLI tool
+          that makes customization and deployment a breeze, but capable of
+          producing even the most complicated sites.
+        </p>
+      </div>
     </div>
   )
 }
